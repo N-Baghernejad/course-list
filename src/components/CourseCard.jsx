0136e6162ff81c0e4fc353d69ee49ea447f74b67
nazanin.b <baghernejad.it89@gmail.com>
2024-09-20 00:26:04 +0330

    extract coursecard component

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,43 +1,63 @@
-function CourseCard(props){
-  console.log(props.course)
+function CourseCard({course}){
     return(
         <div className="course-item">
         <div className="course-item__img">
-          <img src="/images/img1.jpg" alt="image-1" />
+          <img src={course.imageUrl} alt={course.title} />
         </div>
         <div className="course-item__detail">
-          <div className="course-item__body">
-            <div>
-              <p className="title">{props.course.title}</p>
-              <p className="desc">{props.course.description}</p>
-            </div>
-            <span className="rate">{props.course.rate}</span>
-          </div>
-          <div className="course-item__footer">
-            <div className="tags">
-              {
-                props.course.tags.map((t)=> ( 
-                <span key={t} className="badge badge--secondary">
-                  Frontend
-                  </span>
-                  ))
-              }
-              
-            </div>
-            <div className="caption">
-              <div className="date">
-                {new Date(props.course.start).toLocaleDateString("en-US",
-                  {month:"short",
-                  year:"numeric",
-                  day:"numeric",
-                  })}
-              </div>
-              <p className="badge badge--primary">{props.course.status}</p>
-            </div>
-            
-          </div>
+          <CourseCardBody 
+            title={course.title}
+            description={course.description}
+            rate={course.rate}
+           />
+          <CourseCardFooter course={course}/> 
         </div>
         </div>
     )
 }
 export default CourseCard;
+
+function CourseCardBody({title,description,rate}) {
+  return (
+    <div className="course-item__body">
+      <div>
+        <p className="title">{title}</p>
+        <p className="desc">{description}</p>
+      </div>
+      <span className="rate">{rate}</span>
+    </div>
+  )
+}
+
+
+function CourseCardFooter({course}) {
+  return (
+    <div className="course-item__footer">
+      <div className="tags">
+        {
+          course.tags.map((t)=> ( 
+          <span key={t} className="badge badge--secondary">
+            Frontend
+            </span>
+            ))
+        }
+              
+      </div>
+      <div className="caption">
+        <div className="date">
+          {new Date(course.start).toLocaleDateString("en-US",
+            {month:"short",
+            year:"numeric",
+            day:"numeric",
+            })}
+        </div>
+        <p className="badge badge--primary">{course.status}</p>
+      </div>       
+    </div>
+  )
+}
+
+
+
+
+
